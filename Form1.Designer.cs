3d7350c678eb5257a22f78384da5be9e8cb6acce
lyas.daniil@yandex.ru <lyas.daniil@yandex.ru>
2021-06-26 16:47:02 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace laba_7_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DownloadRSS = new System.Windows.Forms.Button();
+            this.UploadDB = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.DownloadBD = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // DownloadRSS
+            // 
+            this.DownloadRSS.Location = new System.Drawing.Point(6, 483);
+            this.DownloadRSS.Name = "DownloadRSS";
+            this.DownloadRSS.Size = new System.Drawing.Size(107, 23);
+            this.DownloadRSS.TabIndex = 0;
+            this.DownloadRSS.Text = "Загрузить RSS";
+            this.DownloadRSS.UseVisualStyleBackColor = true;
+            this.DownloadRSS.Click += new System.EventHandler(this.DownloadRSS_Click);
+            // 
+            // UploadDB
+            // 
+            this.UploadDB.Location = new System.Drawing.Point(484, 492);
+            this.UploadDB.Name = "UploadDB";
+            this.UploadDB.Size = new System.Drawing.Size(128, 23);
+            this.UploadDB.TabIndex = 1;
+            this.UploadDB.Text = "Записать в БД";
+            this.UploadDB.UseVisualStyleBackColor = true;
+            this.UploadDB.Click += new System.EventHandler(this.UploadDB_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(119, 483);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(341, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(15, 19);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(445, 458);
+            this.webBrowser1.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.webBrowser1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.DownloadRSS);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(466, 512);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "WebBrowser";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(484, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(447, 474);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // DownloadBD
+            // 
+            this.DownloadBD.Location = new System.Drawing.Point(618, 492);
+            this.DownloadBD.Name = "DownloadBD";
+            this.DownloadBD.Size = new System.Drawing.Size(128, 23);
+            this.DownloadBD.TabIndex = 6;
+            this.DownloadBD.Text = "Загрузить из БД";
+            this.DownloadBD.UseVisualStyleBackColor = true;
+            this.DownloadBD.Click += new System.EventHandler(this.DownloadBD_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 536);
+            this.Controls.Add(this.DownloadBD);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.UploadDB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button DownloadRSS;
+        private System.Windows.Forms.Button UploadDB;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button DownloadBD;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Xml;
+using System.Data.Common;
+
+namespace laba_7_8
+{
+    public partial class Form1 : Form
+    {
+        public SQLiteConnection db;
+        SQLiteCommand command;
+        public XmlDocument xmlNews;
+        public Form1()
+        {
+            InitializeComponent();
+            db = new SQLiteConnection("Data Source = database.db");
+            db.Open();
+            command = new SQLiteCommand("PRAGMA synchronous = 1; CREATE TABLE IF NOT EXISTS News (Id INTEGER PRIMARY KEY AUTOINCREMENT, Title, Link, Description, PubDate);", db);
+            command.ExecuteNonQuery();
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            db.Close();
+            db.Dispose();
+        }
+        private void DownloadRSS_Click(object sender, EventArgs e)
+        {
+            xmlNews = new XmlDocument();
+            try
+            {
+                xmlNews.Load(textBox1.Text);
+                webBrowser1.Navigate(textBox1.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Некоректный URL");
+            }
+        }
+
+        private void UploadDB_Click(object sender, EventArgs e)
+        {
+            command = new SQLiteCommand("DELETE FROM News", db);
+            command.ExecuteNonQuery();
+            try
+            {
+                XmlNodeList childNodeList = xmlNews.DocumentElement.SelectSingleNode("channel").SelectNodes("item");
+                foreach (XmlNode xmlNode in childNodeList)
+                {
+                    command = new SQLiteCommand("INSERT INTO News(Title,Link,Description,PubDate)VALUES(@title, @link, @description, @pubDate)", db);
+                    command.Parameters.AddWithValue("@title", xmlNode.SelectSingleNode("title").InnerText);
+                    command.Parameters.AddWithValue("@link", xmlNode.SelectSingleNode("link").InnerText);
+                    command.Parameters.AddWithValue("@description", xmlNode.SelectSingleNode("description").InnerText);
+                    command.Parameters.AddWithValue("@pubDate", xmlNode.SelectSingleNode("pubDate").InnerText);
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("XML-документ пуст. Загрузите RSS");
+            }
+
+        }
+        struct MyNews
+        {
+            public string PubDate;
+            public string Title;
+            public string Link;
+            public string Description;
+        }
+        List<MyNews> News = new List<MyNews>();
+        private void DownloadBD_Click(object sender, EventArgs e)
+        {
+            News.Clear();
+            MyNews news = new MyNews();
+            richTextBox1.Text = "";
+            command = new SQLiteCommand("SELECT * FROM News", db);
+            SQLiteDataReader reader = command.ExecuteReader();
+            foreach (DbDataRecord item in reader) 
+            {
+                news.Title = item["Title"].ToString();
+                news.PubDate = item["PubDate"].ToString();
+                news.Link = item["Link"].ToString();
+                news.Description = item["Description"].ToString();
+                richTextBox1.AppendText(new string('=', 70) + "\n");
+                richTextBox1.AppendText(news.PubDate + "\n");
+                richTextBox1.AppendText(news.Title + "\n");
+                richTextBox1.AppendText(news.Link + "\n");
+                richTextBox1.AppendText(news.Description + "\n");
+                richTextBox1.AppendText(new string('=', 70) + "\n");
+                News.Add(news);                  
+            }
+            MessageBox.Show("База данных выгружена. Новостей:"+News.Count);           
+        }
+    }
+}
